aa7a45b4c7de5dfac5a9ff63669913ae664e2f1b
Oscar Dev <scrivimiaoscar@gmail.com>
2016-11-01 12:45:21 +0000

    Added flash to notice the records was saved

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  # Per vedere i routes: bundle exec rake routes
   resources :contacts
-  
+
   get '/about'=>'pages#about'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
